5760a2c48fb1ed0df3c0b9e8a7a1a4ac6db1b972
Rinelfi <elierijaniaina@gmail.com>
2023-12-19 09:55:33 +0300

    group management interface

---
+++ resources/views/applications/group-manage.blade.php
@@ -0,0 +1,14 @@
+@extends('authed')
+@section('title', 'Groupe')
+@section('wrapped-content')
+<div class="card">
+    <div class="card-body pb-0">
+        
+    </div>
+</div>
+@endsection
+@section('dynamic-script')
+<script>
+
+</script>
+@endsection
